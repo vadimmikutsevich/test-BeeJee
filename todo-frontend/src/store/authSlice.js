8cd1f135ca15f42d9be0bb00013897f929a78acc
vadimmikutsevich <vadim.mikutsevich@gmail.com>
2023-07-17 23:42:27 +0400

    optimaze keeping url to request

--- todo-frontend/src/store/authSlice.js
+++ todo-frontend/src/store/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   withCredentials: true
 });
 
